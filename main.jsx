fb1b8f4d7f5d078033b01a38b5baa9bd1c7b4e35
Nally23219 <nally23219@gmail.com>
2026-05-21 21:44:42 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,16 @@
+{
+  "name": "permitiq-client",
+  "private": true,
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build"
+  },
+  "dependencies": {
+    "react": "^18.3.1",
+    "react-dom": "^18.3.1",
+    "axios": "^1.6.8"
+  },
+  "devDependencies": {
+    "vite": "^5.3.0"
+  }
+}
